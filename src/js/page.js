41e899513d4d78c6256c5e24bc3c8895ce5ff854
benjaminv <benvee@gmail.com>
2021-12-16 13:40:34 +1100

    add editing

--- src/js/page.js
+++ src/js/page.js
@@ -125,6 +125,9 @@ function randomIntFromInterval(min, max) {
 
 let startDay = moment().startOf("month").startOf("week").isoWeekday(1);
 
+// var min = new Date(2013, 3, 1); // 1 april
+// var max = new Date(2013, 3, 30, 23, 59, 59); // 30 april
+
 // DOM element where the Timeline will be attached
 var container = document.getElementById('timeline-master');
 
@@ -165,13 +168,128 @@ for (var i = 0; i < itemCount; i++) {
 let options = {
     start: startDay.toDate(),
     end: new Date(1000 * 60 * 60 * 24 + (new Date()).valueOf()),
-    horizontalScroll: true,
+    horizontalScroll: false,
     zoomKey: 'ctrlKey',
     orientation: 'both',
-    zoomMin: 1000 * 60 * 60 * 240
+    zoomMin: 1000 * 60 * 240, // 1 hour
+
+    editable: true,
+
+    onAdd: function (item, callback) {
+      prettyPrompt('Add item', 'Enter text content for new item:', item.content, function (value) {
+        if (value) {
+          item.content = value;
+          callback(item); // send back adjusted new item
+        }
+        else {
+          callback(null); // cancel item creation
+        }
+      });
+    },
+
+    onMove: function (item, callback) {
+      var title = 'Do you really want to move the item to\n' +
+          'start: ' + item.start + '\n' +
+          'end: ' + item.end + '?';
+
+      prettyConfirm('Move item', title, function (ok) {
+        if (ok) {
+          callback(item); // send back item as confirmation (can be changed)
+        }
+        else {
+          callback(null); // cancel editing item
+        }
+      });
+    },
+
+    onMoving: function (item, callback) {
+    //   if (item.start < min) item.start = min;
+    //   if (item.start > max) item.start = max;
+    //   if (item.end   > max) item.end   = max;
+
+      callback(item); // send back the (possibly) changed item
+    },
+
+    onUpdate: function (item, callback) {
+      prettyPrompt('Update item', 'Edit items text:', item.content, function (value) {
+        if (value) {
+          item.content = value;
+          callback(item); // send back adjusted item
+        }
+        else {
+          callback(null); // cancel updating the item
+        }
+      });
+    },
+
+    onRemove: function (item, callback) {
+      prettyConfirm('Remove item', 'Do you really want to remove item ' + item.content + '?', function (ok) {
+        if (ok) {
+          callback(item); // confirm deletion
+        }
+        else {
+          callback(null); // cancel deletion
+        }
+      });
+    }
 };
+
+
 
 // create a Timeline
 var timeline = new vis.Timeline(container, items, groups, options);
 
+/**
+ * Move the timeline a given percentage to left or right
+ * @param {Number} percentage   For example 0.1 (left) or -0.1 (right)
+ */
+    function move (percentage) {
+    var range = timeline.getWindow();
+    var interval = range.end - range.start;
+
+    timeline.setWindow({
+        start: range.start.valueOf() - interval * percentage,
+        end:   range.end.valueOf()   - interval * percentage
+    });
+}
+
+// attach events to the navigation buttons
+document.getElementById('zoomIn').onclick    = function () { timeline.zoomIn( 0.2); };
+document.getElementById('zoomOut').onclick   = function () { timeline.zoomOut( 0.2); };
+document.getElementById('moveLeft').onclick  = function () { move( 0.2); };
+document.getElementById('moveRight').onclick = function () { move(-0.2); };
+document.getElementById('fit').onclick = function() { timeline.fit();  };
+document.getElementById('toggleRollingMode').onclick = function () { timeline.toggleRollingMode() };
+
+// items.on('*', function (event, properties) {
+// logEvent(event, properties);
+// });
+
+// function logEvent(event, properties) {
+// var log = document.getElementById('log');
+// var msg = document.createElement('div');
+// msg.innerHTML = 'event=' + JSON.stringify(event) + ', ' +
+//     'properties=' + JSON.stringify(properties);
+// log.firstChild ? log.insertBefore(msg, log.firstChild) : log.appendChild(msg);
+// }
+
+function prettyConfirm(title, text, callback) {
+swal({
+    title: title,
+    text: text,
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: "#DD6B55"
+}, callback);
+}
+
+function prettyPrompt(title, text, inputValue, callback) {
+swal({
+    title: title,
+    text: text,
+    type: 'input',
+    showCancelButton: true,
+    inputValue: inputValue
+}, callback);
+}
 // add a watch test line
